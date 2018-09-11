88cb8e867fcb471e8709ff5239f11aa40bfa05b9
Dawid Kondracki <dawid.kondracki91@gmail.com>
2018-09-11 19:06:16 +0200

    Added setings and clas databases

--- DEV/DatabasesPeopleWork/DatabasesPeopleWork/DataBases/DataBaseConnectiong.cs
+++ DEV/DatabasesPeopleWork/DatabasesPeopleWork/DataBases/DataBaseConnectiong.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SQLite;
+using System.Windows.Forms;
 
 
 namespace DatabasesPeopleWork.DataBases
 {
     class DataBaseConnectiong
     {
+        public bool chechUserPassword(string user, string password)
+        {
+            using (var con = new SQLiteConnection())
+            {
+                con.ConnectionString = (@"Data Source=C:\Users\d.kondracki\Downloads\DataBaseApllikation-master\DEV\PeopleDatabase\BazaDanych\DBapp.s3db");
+                con.Open();
+                string txtUser = user;
+                string txtPasswd = password;
+                string query = "SELECT * FROM Logowanie WHERE UsersName=@user AND UsersPasswd=@passwd";
+                var cmd = new SQLiteCommand(query,con);
+
+                cmd.Parameters.AddWithValue("@user", txtUser);
+                cmd.Parameters.AddWithValue("@passwd", txtPasswd);
+
+                var dr = cmd.ExecuteReader();
+                if (dr.HasRows == true)
+                {
+
+                    MessageBox.Show("Zalogowano poprawnie");
+                    return true;
+                }
+
+                else
+                {
+                    MessageBox.Show("Błąd logowania");
+                    return false;
+                }
+            }
+        }
     }
 }
